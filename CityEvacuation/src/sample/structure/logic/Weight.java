a012dcfc750e3a526e223b55f11cc39e7d3d14dc
DESKTOP-VHRSBBU\Kuba <jak.pietrzyk.1995@gmail.com>
2017-05-18 20:15:21 +0200

    Weight

---
+++ CityEvacuation/src/sample/structure/logic/Weight.java
@@ -0,0 +1,19 @@
+package sample.structure.logic;
+
+/**
+ * Created by Kuba on 17.05.2017.
+ */
+public class Weight{
+    public static final float door = 34;
+    public static final float downstairs = 571;
+    public static final float flame = 233;
+    public static final float floor = 89;
+    public static final float lawn = 13;
+    public static final float road = 13;
+    public static final float safeZone = 0;
+    public static final float smoke = 144;
+    public static final float upstairs = 571;
+    public static final float windows = 55;
+    public static final float furniture = 571;
+    public static final float wall = 571;
+}
